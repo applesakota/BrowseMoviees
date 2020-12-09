57375272c8a983ea63dc2ae991c810a820938db7
Petar <applesakota@gmail.com>
2020-12-09 13:40:49 +0100

    init

---
+++ BrowseMoviees/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BrowseMoviees
+//
+//  Created by Petar Sakotic on 12/9/20.
+//  Copyright © 2020 Petar Sakotic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    //
+
+}
+
